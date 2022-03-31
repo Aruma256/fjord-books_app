398fe7a231eff014520f3c234853b5e5372c4447
Aruma256 <22253623+Aruma256@users.noreply.github.com>
2022-03-31 11:37:13 +0900

    user/edit viewに追加フィールドの入力欄を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:postcode, :address, :selfintroduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:postcode, :address, :selfintroduction])
   end
 end
