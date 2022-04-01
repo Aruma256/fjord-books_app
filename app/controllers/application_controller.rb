afccd7b2b428ca010c9439561d3f6caef8a7ab02
Aruma256 <22253623+Aruma256@users.noreply.github.com>
2022-04-01 16:38:15 +0900

    rubocop対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,15 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:postcode, :address, :selfintroduction])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:postcode, :address, :selfintroduction])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[postcode address selfintroduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[postcode address selfintroduction])
   end
 
-  def after_sign_in_path_for(resource_or_scope)
+  def after_sign_in_path_for(_resource_or_scope)
     books_path
   end
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_user, only: :show
   before_action :authenticate_user!
 
   # GET /users or /users.json
@@ -8,17 +10,17 @@ class UsersController < ApplicationController
   end
 
   # GET /users/1 or /users/1.json
-  def show
-  end
+  def show; end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.fetch(:user, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.fetch(:user, {})
+  end
 end
