2db32f3e87d772ff922281f4347a6d396319837f
Aruma256 <22253623+Aruma256@users.noreply.github.com>
2022-04-01 11:09:47 +0900

    ログイン・ログアウト後のページ遷移指定を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:postcode, :address, :selfintroduction])
     devise_parameter_sanitizer.permit(:account_update, keys: [:postcode, :address, :selfintroduction])
   end
+
+  def after_sign_in_path_for(resource_or_scope)
+    books_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
